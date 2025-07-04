4d9058e41552713d7935c8c438dc39458bf35f1e
Surya Murugesan <103243861+SuryaDeveloper079@users.noreply.github.com>
2025-07-04 09:36:47 +0530

    Update queue.c

--- Week1/DataStructure/queue.c
+++ Week1/DataStructure/queue.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 
-#define size 5
+#define size 5 // To decleare the Fixed size in Queue
 
-int queue[size];
+int queue[size]; // Create and inilize the Queue and sizeof the Queue
 
-int front=-1,rear=-1;
+int front=-1,rear=-1; //To inilize the front and rear (-1) That means queue is empty
 
-
+//Add the Data to Queue
 void enqueue(int data){
+
+	// To check that rear is size-1 (Queue is full)
 	if(rear == size-1){
 		printf("The Queue is full");
 	}
+
+	// otherwise to add the data into queue
 	else{
 		if (front == -1) front = 0;
 		queue[++rear]=data;
@@ -18,25 +22,38 @@ void enqueue(int data){
 		
 	}
 }
+
+//delete the data from the Queue
 void dequeue(){
+
+	//Check Queue is empty
 	if(front == -1 && front>rear){
 		printf("The Queue is Empty");
 	}
+	//otherwise delete the data from the Queue
 	else{
 		printf("the Deleted Data: %d\n",queue[front++]);
 	}
 }
+
+//Print the Top of the Queue
 void peek(){
 	if(front == -1 && front>rear){
                 printf("The Queue is Empty");
-        }else{
+    }
+	//Print the front Data on the Queue 
+	else{
 		printf("The Top of the Queue : %d",queue[front]);
 	}	
 }
+//Print the all from the Queue
 void display(){
+	//If check Queue is empty or not
 	if(front == -1 && front>rear){
                 printf("The Queue is Empty");
-        }
+    }
+	//To prin the data one by one, iterate the For loop 
+	printf("Queue: ");
 	for(int i=front;i<=rear;i++){
 		printf("%d ",queue[i]);
 	}
@@ -45,16 +62,20 @@ void display(){
 
 int main(){
 
+	//to pass the data to Enque function
 	enqueue(10);
 	enqueue(20);
 	enqueue(30);
-	display();
+
+	display(); // Display the stored Data from the Queue
 	printf("\n");
-	peek();
+	
+	peek(); //Print the Top of the Queue
 	printf("\n");
-	dequeue();
-	printf("\n");
-	peek();
+	dequeue(); // To delete the data Front of the Queue
+	
+	
+
 	printf("\n");
 	display();
 }
